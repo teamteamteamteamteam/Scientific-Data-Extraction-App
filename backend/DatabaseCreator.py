cec819b9c1b1430338e45d973b760c67ce118343
Dorajn <derquv@gmail.com>
2024-11-11 16:24:14 +0100

    working compound fill

--- backend/DatabaseCreator.py
+++ backend/DatabaseCreator.py
@@ -1,14 +1,12 @@
 from pathlib import Path
+from UsablePaths import Paths
 import sqlite3
-
-#Path has got overloaded / operator for differenet os
-backend_folder = Path.cwd() / 'backend' / 'database'
-DATABASE_PATH = backend_folder / 'database.db'
+from DatabaseFiller import DatabaseFiller
 
 class DatabaseCreator:
 
     def __init__(self):
-        self.conn = sqlite3.connect(DATABASE_PATH)
+        self.conn = sqlite3.connect(Paths.DATABASE_PATH)
         self.cursor = self.conn.cursor()
 
     def __del__(self):
@@ -18,12 +16,11 @@ class DatabaseCreator:
     def createTableCompounds(self):
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS Compounds (
-            compound_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            compound_name TEXT NOT NULL,
-            concentration REAL,
-            smiles TEXT,
-            coord_x REAL,
-            coord_y REAL
+                compound_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                compound_name TEXT NOT NULL,
+                smiles TEXT,
+                coord_x REAL,
+                coord_y REAL
             )
         ''')
         self.conn.commit()
@@ -33,6 +30,7 @@ class DatabaseCreator:
             CREATE TABLE IF NOT EXISTS Images (
                 image_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 compound_id INTEGER,
+                concentration REAL,
                 folder_path TEXT,
                 image_path TEXT,
                 FOREIGN KEY (compound_id) REFERENCES Compounds (compound_id)
@@ -44,3 +42,6 @@ class DatabaseCreator:
 database = DatabaseCreator()
 database.createTableCompounds()
 database.createTableImages()
+
+dbFiller = DatabaseFiller(Paths.DATABASE_PATH)
+dbFiller.fill_data_from_images_csv(Paths.COMPOUND_CSV_PATH, 'Compounds')
