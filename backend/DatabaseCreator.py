a9336124d8fc2957faf44f0616aacacf2c7e451b
Wojtek <wojtekmikula1@gmail.com>
2024-11-16 15:14:41 +0100

    Added template method in database management and added coordinates in db

--- backend/DatabaseCreator.py
+++ backend/DatabaseCreator.py
@@ -1,39 +1,18 @@
-from pathlib import Path
-from UsablePaths import Paths
-import sqlite3
-from DatabaseFiller import DatabaseFiller
+from DatabaseInterface import DatabaseInterface
 
 class DatabaseCreator:
 
-    def __init__(self):
-        self.conn = sqlite3.connect(Paths.DATABASE_PATH)
-        self.cursor = self.conn.cursor()
+    def __init__(self, database: DatabaseInterface):
+        self.database = database
+        self.database.connect()
 
     def __del__(self):
-        self.cursor.close()
-        self.conn.close()
+        self.database.close()
 
     def create_table_compounds(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS Compounds (
-                compound_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                compound_name TEXT NOT NULL,
-                smiles TEXT,
-                coord_x REAL,
-                coord_y REAL
-            )
-        ''')
-        self.conn.commit()
+        self.database.create_table_compounds()
+        self.database.commit()
 
     def create_table_images(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS Images (
-                image_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                compound_id INTEGER,
-                concentration REAL,
-                folder_path TEXT,
-                image_path TEXT,
-                FOREIGN KEY (compound_id) REFERENCES Compounds (compound_id)
-            )
-        ''')
-        self.conn.commit()
+        self.database.create_table_images()
+        self.database.commit()
