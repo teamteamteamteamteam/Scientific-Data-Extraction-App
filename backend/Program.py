e4fd86a3668597d1f2b085b68dbb245fad4f4da9
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2024-11-24 15:36:35 +0100

    update to match current version

--- backend/Program.py
+++ backend/Program.py
@@ -4,15 +4,22 @@ from UsablePaths import Paths
 from SQLiteDatabase import SQLiteDatabase
 from CoordinatesCreator import CoordinatesCreator
 
-database = SQLiteDatabase(Paths.DATABASE_PATH)
 
+
+database = SQLiteDatabase(Paths.DATABASE_PATH / 'database.db')
+
+# Tworzenie tabel
 db_creator = DatabaseCreator(database)
-db_creator.create_table_compounds()
-db_creator.create_table_images()
+db_creator.create_table('compounds')
+db_creator.create_table('images')
 
-db_filler = DatabaseFiller(database)
-db_filler.fill_compounds_table(Paths.COMPOUND_CSV_PATH)
-db_filler.fill_images_table(Paths.IMAGES_CSV_PATH)
+# Wypełnianie tabeli związków chemicznych
+db_filler_compounds = CompoundsDatabaseFiller(database)
+db_filler_compounds.fill_data_from_csv(Paths.COMPOUND_CSV_PATH, 'compounds')
+
+# Wypełnianie tabeli obrazów
+db_filler_images = ImagesDatabaseFiller(database)
+db_filler_images.fill_data_from_csv(Paths.IMAGES_CSV_PATH, 'images')
 
 db_coordinates = CoordinatesCreator(database)
 db_coordinates.calculate_coordinates(Paths.COMPOUND_CSV_PATH)
