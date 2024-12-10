0227dfa73f6603637d497ea503d051b215373f9d
Wojtek <wojtekmikula1@gmail.com>
2024-12-10 17:52:56 +0100

    fixed problem with coordinates

--- backend/utils/Program.py
+++ backend/utils/Program.py
@@ -7,16 +7,16 @@ from pathlib import Path
 
 database = SQLiteDatabase(Paths.DATABASE_PATH)
 
-# # Creating tables
-# db_creator = DatabaseCreator(database)
-# db_creator.create_table('compounds')
-# db_creator.create_table('images')
-# db_creator.create_table('color_by_concentration')
-# db_creator.create_table('color_by_moa')
+# Creating tables
+db_creator = DatabaseCreator(database)
+db_creator.create_table('compounds')
+db_creator.create_table('images')
+db_creator.create_table('color_by_concentration')
+db_creator.create_table('color_by_moa')
 
-# # Filling tables with initial data
-# db_filler_compounds = DatabaseFiller(database)
-# db_filler_compounds.fill_initial_data()
+# Filling tables with initial data
+db_filler_compounds = DatabaseFiller(database)
+db_filler_compounds.fill_initial_data()
 
 formatted_folder_path = Path(__file__).parent / "formatter_csv" / "formatted"
 original_folder_path = Path(__file__).parent / "formatter_csv" / "original"
