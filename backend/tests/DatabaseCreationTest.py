07db07a115675ef875afb17626b8d1a00f8fbc2a
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2024-12-17 18:14:36 +0100

    import as modules, run with python -m backend.utils.program

--- backend/tests/DatabaseCreationTest.py
+++ backend/tests/DatabaseCreationTest.py
@@ -1,8 +1,10 @@
 import sqlite3
 import pytest
+
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from backend.utils.DatabaseCreator import DatabaseCreator
 
 @pytest.fixture
