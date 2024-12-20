229e0a763aa4ceaf4a464dc919af3da8e48ae924
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2024-12-20 15:03:11 +0100

    space

--- backend/tests/test_UsablePaths.py
+++ backend/tests/test_UsablePaths.py
@@ -3,7 +3,6 @@ import pytest
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from backend.utils.UsablePaths import Paths
 
 @pytest.fixture
