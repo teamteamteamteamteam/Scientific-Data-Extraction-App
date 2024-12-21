c620f58aea7c007c49dca9c19f73f6c220be04fd
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2024-12-21 02:17:46 +0100

    added: # pragma: no cover for lines with pass keyword

--- backend/utils/DatabaseInterface.py
+++ backend/utils/DatabaseInterface.py
@@ -3,80 +3,80 @@ from abc import ABC, abstractmethod
 class DatabaseInterface(ABC):
     @abstractmethod
     def connect(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def close(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def commit(self):
-        pass 
+        pass # pragma: no cover
 
     @abstractmethod
     def create_table_compounds(self):
-        pass
+        pass # pragma: no cover
     
     @abstractmethod
     def insert_into_table_compounds(self, compound_name, compound_concentration, smiles, is_active):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def update_compounds_moa(self, compound_name, moa_id):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def updata_compounds_empty_moa(self, moa_id):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def update_compound_coordinates(self, compound_id, new_x, new_y, is_active):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def fetch_compound_by_name_and_concentration(self, compound_name, concentration):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def fetch_all_compounds(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def fetch_all_compounds_colored_by_concentration(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def fetch_all_compounds_colored_by_moa(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def fetch_compound_details(self, compound_name, compound_concentration):
-        pass
+        pass # pragma: no cover
     
     @abstractmethod
     def create_table_images(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def insert_into_table_images(self, compound_id, folder_path, dapi, tubulin, actin):
-        pass   
+        pass # pragma: no cover
 
     @abstractmethod
     def create_table_color_by_concentration(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def insert_into_color_by_concentration(self, r, g, b):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def create_table_color_by_moa(self):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def update_compounds_color_concentration(self, concentration, color_id):
-        pass
+        pass # pragma: no cover
 
     @abstractmethod
     def insert_into_color_table_by_moa(self, moa, concentration, r, g, b):
-        pass
+        pass # pragma: no cover
